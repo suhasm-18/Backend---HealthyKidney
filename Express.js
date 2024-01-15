18df08498728d6f0bc1daa4ba9deb6986d18bc61
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2024-01-15 12:23:47 +0530

    Create Express.js

---
+++ Express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
+    }]
+}];
+
+app.use(express.json());
+
