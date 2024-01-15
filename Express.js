662d363e9c6d77f080d9c2d377e0138b6c616a6f
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2024-01-15 12:24:07 +0530

    Update Express.js

--- Express.js
+++ Express.js
@@ -9,4 +9,21 @@ const users = [{
 }];
 
 app.use(express.json());
+app.get("/", function(req, res) {
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i<johnKidneys.length; i++) {
+        if (johnKidneys[i].healthy) {
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+})
+
 
